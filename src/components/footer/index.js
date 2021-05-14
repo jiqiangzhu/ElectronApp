ebb1c7be9e80156b903bdd1eccd6d9e51ff3f56f
blairq <to_zjq_vip@163.com>
2021-05-14 11:17:39 +0800

    播放进度条完善

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,39 +8,51 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 export default function FooterCom(props) {
     useEffect(() => {
-
+        setDuration(audioRef.current.duration)
     })
     const IconFont = createFromIconfontCN();
-    // const [beginTime, setBeginTime] = useState(0);
+    const [beginTime, setBeginTime] = useState(0);
     const [loopFlag, setLoopFlag] = useState(true);
     // const [currentIndex, setCurrentIndex] = useState(1);
     const audioRef = React.createRef();
-    let [playFlag, setPlayFlag] = useState("play");
+    const progressRef = React.createRef();
+    const [playFlag, setPlayFlag] = useState("play");
+    const [duration, setDuration] = useState(0);
     // const getDuration = () => {
 
     // }
-    // const updateTime = value => {
-    //     console.log("播放到时间----", value);
-    //     setBeginTime(value)
-    // }
+    const [persent, setPersent] = useState(0);
+
+    const updateTime = () => {
+        // const duration = audioRef.current.duration;
+        let temPersent = (audioRef.current.currentTime / duration) * 100;
+        setPersent(temPersent)
+        setBeginTime(parseInt(audioRef.current.currentTime))
+
+    }
     const setPlayMode = () => {
         console.log("this", this);
         setLoopFlag(!loopFlag);
         console.log("当前播放模式 单曲循环 true  否则 false", loopFlag);
     }
     const playMusic = (flag) => {
-        console.log("flag------------", typeof flag);
         setPlayFlag(flag);
-        console.log("playFlag----------", typeof playFlag);
         if (playFlag === "play") {
-            console.log("......play");
             audioRef.current.play();
         } else if (playFlag === "pause") {
             audioRef.current.pause();
         }
     }
 
-
+    const setCurrentPlayTime = (event) => { //205
+        console.log("event---------", event.pageX);
+        console.log("progressRef.current.offsetLeft--------", progressRef.current.offsetLeft + 205);
+        console.log("progressRef.current.width--------", progressRef.current.offsetWidth + 205);
+        let currentProgress = event.pageX - (progressRef.current.offsetLeft + 205);
+        let currentRate = parseInt(currentProgress / progressRef.current.offsetWidth * 100);
+        let setCurrentTime = (duration * currentRate) / 100
+        audioRef.current.currentTime = setCurrentTime;
+    }
     // const playNext = () => {
     //     setCurrentIndex(currentIndex + 1)
     // }
@@ -48,16 +60,15 @@ export default function FooterCom(props) {
     return (
         <>
             {/* 
-        
                 onCanPlay={getDuration.bind(this)}
                 onTimeUpdate={updateTime.bind(this)} 
-                
                 onEnded={playNext.bind(this, currentIndex)}    
             */}
             <audio
+                onTimeUpdate={updateTime.bind(this)}
                 ref={audioRef}
-                loop={loopFlag}
                 preload="true"
+                loop={loopFlag}
                 controls={false}
                 src={mp3Path}
             ></audio>
@@ -71,21 +82,22 @@ export default function FooterCom(props) {
                         <StepForwardOutlined style={{ fontSize: "24px", cursor: "pointer" }} />
                     </Space>
                 </Col>
-                <Col span={1}>
-                    {/* {beginTime} */}
+                <Col span={1} style={{ paddingBottom: '10px', paddingRight: '10px' }} className="flex-type flex-justify-end">
+                    {secondsFormat(beginTime)}
                 </Col>
-                <Col offset={1} span={12} >
-                    <Progress percent={100} className="audio-process" showInfo={false} strokeColor={{
-                        '0%': '#108ee9',
-                        '100%': '#87d068',
-                    }} />
+                <Col span={12}>
+                    <div ref={progressRef}>
+                        <Progress percent={persent} onClick={setCurrentPlayTime.bind(this)} className="audio-process" showInfo={false} strokeColor={{
+                            '0%': '#108ee9',
+                            '100%': '#87d068',
+                        }} />
+                    </div>
                 </Col>
-                <Col offset={1} span={1}>
-
+                <Col style={{ paddingBottom: '10px', paddingLeft: '10px' }} span={1}>
+                    {secondsFormat(parseInt(duration) ? parseInt(duration) : 0)}
                 </Col>
-                <Col span={2}>
-                    <IconFont type="icon-hanhan-01-011" onClick={setPlayMode.bind(this)} className="webkit-no-drag" />
-
+                <Col span={2} className="flex-type flex-justify-end">
+                    <IconFont style={{ paddingBottom: '10px', fontSize: '16px' }} type="icon-hanhan-01-011" onClick={setPlayMode.bind(this)} className="webkit-no-drag" />
                 </Col>
             </Row>
 
@@ -106,7 +118,21 @@ function PlayStatusCom(props) {
     }
 
 }
-
+function secondsFormat(sec){
+    let hour = Math.floor(sec / 3600);
+    let minute = Math.floor((sec - hour * 3600) / 60);
+    let second = sec - hour * 3600 - minute * 60;
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+    return minute + ":" + second;
+ }
 // function Counter(props) {
 //     const [count, setCount] = useState(props.initialCount);
 //     return (
