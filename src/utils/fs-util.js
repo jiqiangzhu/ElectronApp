760e03b0d9cfc54e41d9880d5d2c3ffa47fa75c5
blairq <to_zjq_vip@163.com>
2021-05-18 18:44:58 +0800

    重命名文件夹

---
+++ src/utils/fs-util.js
@@ -0,0 +1,21 @@
+/**
+ * 本地文件工具
+ */
+
+const fs = window.require('fs-extra');
+const fsUtils = {
+    /**
+     * 读取路径中所有文件
+     * @param {*} path 文件夹路径
+     * @param {*} resolve 回调函数
+     * @returns 
+     */
+    readMusicDir: async (path, resolve) => {
+        if (!path) {
+            return;
+        }
+        fs.readdir(path, resolve)
+    }
+}
+
+export default fsUtils;
