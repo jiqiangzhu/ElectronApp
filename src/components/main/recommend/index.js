4bc4dd6e7dcc14a94012542bade1a5689a0a37d8
blairq <to_zjq_vip@163.com>
2021-07-30 10:46:19 +0800

    recommend images

--- src/components/main/recommend/index.js
+++ src/components/main/recommend/index.js
@@ -1,7 +1,102 @@
+import { Row, Col, Image } from 'antd';
+import { useEffect } from 'react';
+import './index.less';
+
 function Recommend(props) {
+    const imgPath = require('@/assets/img/recommend/2.jpg').default;
+    useEffect(() => {
+
+    }, [])// eslint-disable-line react-hooks/exhaustive-deps
     return (
-        <div className="home-content">
-            
+        <div className="home-content recommend">
+            <div className="scroller-bar">
+                <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                    <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 1 }}>
+                        <Image
+                            src={imgPath}
+                        />
+                    </Col>
+                </Row>
+
+            </div>
         </div>
     )
 }
