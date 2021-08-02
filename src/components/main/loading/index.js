be8fe6a9b6558754c0f90922574ca6c4b921c7b1
blairq <to_zjq_vip@163.com>
2021-08-02 20:19:11 +0800

    loading dialog

--- src/components/main/loading/index.js
+++ src/components/main/loading/index.js
@@ -1,24 +1,36 @@
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { setShowLoaingRedux } from 'src/redux/actions/play-actions';
 import './index.less';
-import { Button } from 'antd';
 
 function Loading(props) {
     const { showLoading, setShowLoading } = props;
+    const [showTips, setShowTips] = useState("Loading...")
+    useEffect(() => {
+        setTimeout(() => {
+            setShowTips("Cancel");
+        })
+    }, [showTips])
     return (
-        <div className="loading" style={{ display: showLoading ? "flex" : "none" }}>
-            <div className="solar">
-                <i className="mercury"></i>
-                <i className="venus"></i>
-                <i className="earth"></i>
-                <i className="mars"></i>
-                <i className="belt"></i>
-                <i className="jupiter"></i>
-                <i className="saturn"></i>
-                <i className="uranus"></i>
-                <i className="neptune"></i>
+        <div className="loading cannotselect" style={{ display: showLoading ? "flex" : "none" }}>
+            <div className="box">
+                <div className="solar">
+                    <i className="mercury"></i>
+                    <i className="venus"></i>
+                    <i className="earth"></i>
+                    <i className="mars"></i>
+                    <i className="belt"></i>
+                    <i className="jupiter"></i>
+                    <i className="saturn"></i>
+                    <i className="uranus"></i>
+                    <i className="neptune"></i>
+                </div>
+                <div className="tips">
+                    <div className={`words ${showTips === "Cancel" ? "cancel" : "continue"}`} onClick={() => setShowLoading(false)}>
+                        {showTips}
+                    </div>
+                </div>
             </div>
-            <Button onClick={() => setShowLoading(showLoading)}>关闭</Button>
         </div>
     )
 }
@@ -30,7 +42,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         setShowLoading: (showLoading) => {
-            dispatch(setShowLoaingRedux(!showLoading))
+            dispatch(setShowLoaingRedux(showLoading))
         }
     }
 }
