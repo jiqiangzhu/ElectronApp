5108785f1010254b64805e717ec880036c0a78cc
blairq <to_zjq_vip@163.com>
2021-08-09 22:17:35 +0800

    new animation

--- src/router/second-router.js
+++ src/router/second-router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { ChinaMapCom } from 'src/components/main/echarts';
+import LeaderBoard from 'src/components/main/leader-board';
 import MenuBar from 'src/components/main/menu-bar';
 import Movie from 'src/components/main/movie';
 import Recommend from 'src/components/main/recommend';
@@ -13,6 +14,7 @@ const routes2 = (
             <Route path="/home" component={Recommend}></Route>
             <Route exact path="/fymap" component={ChinaMapCom} />
             <Route exact path="/movie" component={Movie} />
+            <Route exact path="/rank" component={LeaderBoard} />
             <Route component={NotFound} />
         </Switch>
     </HashRouter>
