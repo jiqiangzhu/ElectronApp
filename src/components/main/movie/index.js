03d15c477ce7965035316bbb3b956e2369ea6374
blairq <to_zjq_vip@163.com>
2021-08-09 08:27:25 +0800

    css3 animation for photo

--- src/components/main/movie/index.js
+++ src/components/main/movie/index.js
@@ -1,10 +1,67 @@
-import './index.less'
+import './index.less';
+import './index.scss';
 
 function Movie(props) {
 
     return (
         <div className="home-content movie-content">
+            <div class="page">
 
+                <div class="container">
+
+                    <div class="photo-container">
+                        <div class="photo-cont-item animation-1">
+                            <div class="photo-item photo-1"></div>
+                            <div class="photo-item photo-2"></div>
+                            <div class="photo-item photo-3"></div>
+                            <div class="photo-item photo-4"></div>
+                            <div class="photo-item photo-5"></div>
+                            <div class="photo-item photo-6"></div>
+
+                            <div class="photo-item photo-1"></div>
+                            <div class="photo-item photo-2"></div>
+                            <div class="photo-item photo-3"></div>
+                            <div class="photo-item photo-4"></div>
+                            <div class="photo-item photo-5"></div>
+                            <div class="photo-item photo-6"></div>
+                        </div>
+
+                        <div class="photo-cont-item animation-2">
+                            <div class="photo-item photo-7"></div>
+                            <div class="photo-item photo-8"></div>
+                            <div class="photo-item photo-9"></div>
+                            <div class="photo-item photo-10"></div>
+                            <div class="photo-item photo-11"></div>
+                            <div class="photo-item photo-12"></div>
+
+                            <div class="photo-item photo-7"></div>
+                            <div class="photo-item photo-8"></div>
+                            <div class="photo-item photo-9"></div>
+                            <div class="photo-item photo-10"></div>
+                            <div class="photo-item photo-11"></div>
+                            <div class="photo-item photo-12"></div>
+                        </div>
+
+                        <div class="photo-cont-item animation-3">
+                            <div class="photo-item photo-13"></div>
+                            <div class="photo-item photo-14"></div>
+                            <div class="photo-item photo-15"></div>
+                            <div class="photo-item photo-16"></div>
+                            <div class="photo-item photo-17"></div>
+                            <div class="photo-item photo-18"></div>
+
+                            <div class="photo-item photo-13"></div>
+                            <div class="photo-item photo-14"></div>
+                            <div class="photo-item photo-15"></div>
+                            <div class="photo-item photo-16"></div>
+                            <div class="photo-item photo-17"></div>
+                            <div class="photo-item photo-18"></div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
