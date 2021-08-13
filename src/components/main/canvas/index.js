bb9d730ef78f38f66a8ff95b4a2fa278e5df2d6e
blairq <to_zjq_vip@163.com>
2021-08-13 21:51:28 +0800

    clock of canvas

--- src/components/main/canvas/index.js
+++ src/components/main/canvas/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './index.scss';
 
 function Clock() {
     const clockRef = React.createRef();
@@ -14,96 +15,106 @@ function Clock() {
 
         function draw() {
             try {
-            var canvas = clockRef.current;
-            if (!canvas.getContext) return;
-            var ctx = canvas.getContext("2d");
-            ctx.clearRect(0, 0, 400, 400);
-            ctx.save();
-            ctx.translate(200, 200);
-            let timer = new Date();
-            let sec = timer.getSeconds();
-            let min = timer.getMinutes();
-            let hour = timer.getHours();
-            // 绘制秒针
-            ctx.save();
-            ctx.rotate(Math.PI * 2 * sec / 60);
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, -105);
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "green";
-            ctx.closePath();
-            ctx.stroke();
-            ctx.restore();
-            //绘制分针
-            ctx.save();
-            ctx.rotate(Math.PI * 2 * min / 60);
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, -80);
-            ctx.strokeStyle = 'orange';
-            ctx.lineWidth = 3;
-            ctx.closePath();
-            ctx.stroke();
-            ctx.restore();
-            //绘制时针
-            ctx.save();
-            ctx.rotate(Math.PI * 2 * hour / 12);
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(0, -70);
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = 5;
-            ctx.closePath();
-            ctx.stroke();
-            ctx.restore();
-            //中心的原点
-            ctx.beginPath();
-            ctx.arc(0, 0, 2, 0, 2 * Math.PI);
-            ctx.strokeStyle = "red";
-            ctx.stroke();
-            //绘制表盘
-            for (let i = 1; i < 13; i++) {
+                var canvas = clockRef.current;
+                if (!canvas.getContext) return;
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect(0, 0, 480, 480);
                 ctx.save();
-                ctx.rotate(Math.PI * 2 * i / 12);
+                ctx.translate(240, 240);
+                let timer = new Date();
+                let sec = timer.getSeconds();
+                let min = timer.getMinutes();
+                let hour = timer.getHours();
+                // 绘制表框
                 ctx.beginPath();
-                ctx.moveTo(0, 100);
-                ctx.lineTo(0, 110);
-                ctx.strokeStyle = 'blue';
-                ctx.lineWidth = i % 3 ? 2 : 4;
-                ctx.strokeStyle = i % 3 ? "blue" : "red";
-                ctx.closePath();
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = '#333';
+                ctx.arc(0, 0, 208, 0, 2 * Math.PI, false);
                 ctx.stroke();
-                ctx.restore();
-            }
 
-            for (let i = 1; i < 13; i++) {
+                // 绘制秒针
                 ctx.save();
-                ctx.rotate(Math.PI * 2 * i / 12);
+                ctx.rotate(Math.PI * 2 * sec / 60);
                 ctx.beginPath();
-                ctx.moveTo(0, 100);
-                ctx.font = "bold 14px Arial";
-                ctx.textAlign = "center";
-                ctx.textBaseline = "middle";
-                ctx.fillText(i, 120, 0);
+                ctx.moveTo(0, 0);
+                ctx.lineTo(0, -168);
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = "green";
                 ctx.closePath();
                 ctx.stroke();
                 ctx.restore();
+                //绘制分针
+                ctx.save();
+                ctx.rotate(Math.PI * 2 * min / 60);
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo(0, -160);
+                ctx.strokeStyle = 'orange';
+                ctx.lineWidth = 3;
+                ctx.closePath();
+                ctx.stroke();
+                ctx.restore();
+                //绘制时针
+                ctx.save();
+                ctx.rotate(Math.PI * 2 * hour / 12);
+                ctx.beginPath();
+                ctx.moveTo(0, 0);
+                ctx.lineTo(0, -112);
+                ctx.strokeStyle = 'red';
+                ctx.lineWidth = 5;
+                ctx.closePath();
+                ctx.stroke();
+                ctx.restore();
+                //中心的原点
+                ctx.beginPath();
+                ctx.arc(0, 0, 2, 0, 2 * Math.PI);
+                ctx.strokeStyle = "red";
+                ctx.stroke();
+                //绘制表盘
+                for (let i = 1; i < 61; i++) {
+                    ctx.save();
+                    ctx.rotate(Math.PI * 2 * i / 60);
+                    ctx.beginPath();
+                    ctx.moveTo(0, 160);
+                    ctx.lineTo(0, 176);
+                    ctx.strokeStyle = 'blue';
+                    ctx.lineWidth = i % 5 ? 1 : 4;
+                    ctx.strokeStyle = i % 5 ? "#999" : "#fff";
+                    ctx.closePath();
+                    ctx.stroke();
+                    ctx.restore();
+                }
+
+                for (let i = 1; i < 13; i++) {
+                    ctx.save();
+                    ctx.rotate(Math.PI * 2 * i / 12);
+                    ctx.beginPath();
+                    ctx.moveTo(0, 160);
+                    ctx.fillStyle = '#fff'
+                    ctx.font = "bold 14px Arial";
+                    ctx.textAlign = "center";
+                    ctx.textBaseline = "middle";
+                    ctx.fillText(i, 192, 0);
+                    ctx.closePath();
+                    ctx.stroke();
+                    ctx.restore();
+                }
+                ctx.restore();
+                window.requestAnimFrame(draw);
+            } catch (e) {
+                console.warn('clock', e);
             }
-            ctx.restore();
-            window.requestAnimFrame(draw);
-        } catch(e) {
-            console.warn('clock', e);
-        }
         };
         window.requestAnimFrame(draw);
     }
-    useEffect(()=>{
+    useEffect(() => {
         drawCanvas();
     }, [])
     return (
         <div className="home-content movie-content">
-            <canvas ref={clockRef} id="clock" width="400" height="400"></canvas>
+            <div className="canvas-box">
+                <canvas ref={clockRef} id="clock" width="480" height="480"></canvas>
+            </div>
         </div>
     )
 }
--- src/router/second-router.js
+++ src/router/second-router.js
@@ -12,7 +12,7 @@ const routes2 = (
     <HashRouter>
         <Route component={MenuBar} />
         <Switch>
-            <Route path="/home" component={Recommend}></Route>
+            <Route path="/home" component={Clock}></Route>
             <Route exact path="/fymap" component={ChinaMapCom} />
             <Route exact path="/movie" component={Movie} />
             <Route exact path="/rank" component={LeaderBoard} />
